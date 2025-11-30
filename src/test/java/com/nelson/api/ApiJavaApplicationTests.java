3c5ddedd4d7a7441175d5671c549230ddcec5cf1
nelsonEGR <nelsoongoonzalez@gmail.com>
2025-11-30 13:47:12 -0300

    primera api configuracion

---
+++ src/test/java/com/nelson/api/ApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelson.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
